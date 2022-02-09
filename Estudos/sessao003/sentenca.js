e63ffdc4b8641c912b65b2f93d9f82536dcaff23
IsraFerreira <israel.fnds@hotmail.com>
2022-02-09 16:03:03 -0300

    att

---
+++ Estudos/sessao003/sentenca.js
@@ -0,0 +1,16 @@
+// console.log é usado para exibir algo na tela do computador! 
+console.log('Passo #01');
+console.log('Passo #02');
+console.log('Passo #03');
+
+/*
+    comentário de várias linhas 
+*/
+
+/*
+ * Outra forma de mostrar um comentário de várias linhas
+ *
+ * 
+ * 
+ * 
+ */
