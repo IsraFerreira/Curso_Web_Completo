6d4d026802a6305a3984ff2d93ba1b038fe9687a
IsraFerreira <israel.fnds@hotmail.com>
2022-04-08 15:20:35 -0300

    att

---
+++ Estudos/Exercicios001/ex10.js
@@ -0,0 +1,14 @@
+function divisivel(n){
+    if(n % 3 == 0){
+        console.log(`${n} é divisível por 3`)
+    }
+    else {
+        console.log(`${n} não é divisível por 3`)  
+    }
+}
+
+divisivel(3)
+divisivel(6)
+divisivel(9)
+divisivel(4)
+divisivel(5)
