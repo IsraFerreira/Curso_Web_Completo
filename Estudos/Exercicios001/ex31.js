7b283d3ba56466bca780108ee20d84fb98845de0
IsraFerreira <israel.fnds@hotmail.com>
2022-04-27 16:31:55 -0300

    Create ex31.js

---
+++ Estudos/Exercicios001/ex31.js
@@ -0,0 +1,10 @@
+// Escrever um algoritmo que percorre um vetor de inteiros, conta quantos números negativos há nesse vetor
+// e imprime a quantidade no console.
+
+function negativos(x){
+    for(let i = 0; i < x.length; i++){
+        if(x[i] )
+    }
+}
+
+negativos(vetor = [-5, -10, 2, 4, 8, -3])
