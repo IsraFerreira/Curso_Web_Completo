8db36f47ba87190364aad3b8c0afddc97414c41b
IsraFerreira <israel.fnds@hotmail.com>
2022-04-29 13:38:13 -0300

    att

--- Estudos/Exercicios001/ex33.js
+++ Estudos/Exercicios001/ex33.js
@@ -3,3 +3,19 @@
 // Declarados os vetores, utilize a função de união concat() de duas maneiras diferentes para unir os vetores, e
 // mostre o resultado no console. Todos os elementos do vetor resultado deverão aparecer no console.
 
+function vetores(inteiro, string, double){
+    let resultado = []
+    for(let i=0; i < arguments.length; i++){
+        resultado = resultado.concat(arguments[i])
+    }
+    return console.log(`${resultado}`)
+}
+
+
+
+vetorInteiro = [1, 2, 3, 4]
+vetorString = ['Ana', 'Pedro', 'Carlos', 'Israel']
+vetorDouble = [1.5, 2.3, 3.7, 5.6]
+
+vetores(vetorInteiro, vetorString, vetorDouble)
+
