2c1467de1682f27d8c6771e0bbfcad0a9f9c2663
IsraFerreira <israel.fnds@hotmail.com>
2022-05-02 11:09:14 -0300

    att

---
+++ Estudos/sessao007 Objeto/OO.js
@@ -0,0 +1,21 @@
+//Código não executável
+
+//Procedural
+processamento (valor1, valor2, valor3)
+
+//OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento() //foco passou a ser o objeto
+
+
+
+//Pontos importanes da linguagem orientada a objeto
+// 1. Abstração
