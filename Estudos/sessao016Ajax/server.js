4b782734f4cf4ebb0107565d270f7a7db0eda225
IsraFerreira <israel.fnds@hotmail.com>
2022-07-29 14:22:30 -0300

    Update server.js

--- Estudos/sessao016Ajax/server.js
+++ Estudos/sessao016Ajax/server.js
@@ -6,5 +6,5 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.get('/teste', (req, res) => res.send('Ok')) //quando vier uma requisição pela URL /teste 
+app.get('/teste', (req, res) => res.send(new Date)) //quando vier uma requisição pela URL /teste 
 app.listen(8080, () => console.log('Executando...'))
