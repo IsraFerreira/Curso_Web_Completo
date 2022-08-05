701ee07665921eb8833308bb56d7d43b699406aa
IsraFerreira <israel.fnds@hotmail.com>
2022-08-05 09:58:20 -0300

    att

--- Estudos/sessao017Gulp/basico/gulpfile.js
+++ Estudos/sessao017Gulp/basico/gulpfile.js
@@ -1,11 +1,36 @@
 const gulp = require('gulp')
+const {series, parallel} = require('gulp')
 // const { series } = require('gulp') //fazer varias tarefas em série
 
-const series = gulp.series 
+// const series = gulp.series 
 
-function copiar(cb){
-    console.log('Tarefa de copiar!')
+const antes1 = cb => {
+    console.log('Tareja Antes 1!')
     return cb()
 }
 
-module.exports.default = series(copiar)
+const antes2 = cb => {
+    console.log('Tareja Antes 2!')
+    return cb()
+}
+
+function copiar(cb){
+    //gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt']) //seleciona arquivos como insumo (entrada) no workflow
+
+    gulp.src('pastaA/**/*.txt') //qualquer arquivo na pastaA que tenha .txt via
+        .pipe(gulp.dest('pastaB')) //faz transformação
+        // .pipe(imagePelaMetade())
+        // .pipe(imageEmPetroEBranco())
+        // .pipe(transformacaoA())
+        // .pipe(transformacaoB())
+        // .pipe(transformacaoC())
+ 
+    return cb()
+}
+
+const fim = cb => {
+    console.log('Tareja Fim!')
+    return cb()
+}
+
+module.exports.default = series(parallel(antes1, antes2), copiar, fim)
