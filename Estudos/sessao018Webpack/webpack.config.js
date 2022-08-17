c2c0c14609efc1efa021a7978cd49484ba196752
IsraFerreira <israel.fnds@hotmail.com>
2022-08-17 15:08:19 -0300

    att

--- Estudos/sessao018Webpack/webpack.config.js
+++ Estudos/sessao018Webpack/webpack.config.js
@@ -1,15 +1,29 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
     },
+    optimization: {
+        minimizer: [
+            
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
     plugins: [
+        new TerserPlugin({
+            parallel: true,
+            terserOptions: {
+                ecma: 6,
+            },
+        }),
         new MiniCssExtractPlugin({
             filename: "estilo.css"
         })
