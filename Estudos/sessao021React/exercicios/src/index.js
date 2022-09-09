a8ca64cdf4240e96ef47f08dec1963c9416be1b9
Israel Ferreira <israel.fnds@hotmail.com>
2022-09-09 12:48:18 -0300

    att

--- Estudos/sessao021React/exercicios/src/index.js
+++ Estudos/sessao021React/exercicios/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import BomDia from './componentes/BomDia'
-import Primeiro from './componentes/Primeiro'
+import Multi, { BoaNoite } from './componentes/Multiplos'
 
-ReactDOM.render(<BomDia nome="Guilherme" />, document.getElementById('root'))
+
+
+ReactDOM.render(
+    <div>
+        <Multi.BoaTarde nome="Ana" />
+        <BoaNoite nome="Bia" />
+    </div>
+    , document.getElementById('root'))
 
