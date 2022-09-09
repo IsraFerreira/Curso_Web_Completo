cc7337a55182834cad9a7e2374e8601b24d622c7
Israel Ferreira <israel.fnds@hotmail.com>
2022-09-09 12:36:46 -0300

    att

---
+++ Estudos/sessao021React/exercicios/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import BomDia from './componentes/BomDia'
+import Primeiro from './componentes/Primeiro'
+
+ReactDOM.render(<BomDia nome="Guilherme" />, document.getElementById('root'))
+
