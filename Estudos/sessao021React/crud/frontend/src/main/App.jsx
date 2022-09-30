a75381c378a516668a942ad63448bc89469dc319
Israel Ferreira <israel.fnds@hotmail.com>
2022-09-30 09:57:31 -0300

    att

--- Estudos/sessao021React/crud/frontend/src/main/App.jsx
+++ Estudos/sessao021React/crud/frontend/src/main/App.jsx
@@ -3,13 +3,14 @@ import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
-// import { HashRouter } from 'react-router-dom'
 
 
-import Logo from '../components/template/Logo'
-import Nav from '../components/template/Nav'
-import Routes from './Routes'
-import Footer from '../components/template/Footer'
+import Logo from '../components/template/Logo';
+import Nav from '../components/template/Nav';
+import Routes from './Routes';
+import Footer from '../components/template/Footer';
+
+
 
 export default props =>
  <BrowserRouter>
@@ -20,4 +21,6 @@ export default props =>
         <Footer />
     </div>
  </BrowserRouter> 
+
+
  
