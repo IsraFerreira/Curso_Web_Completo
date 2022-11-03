6331e4cde87afa553d6096ed5ce06b2e47bbb060
Israel Ferreira <israel.fnds@hotmail.com>
2022-11-03 11:24:20 -0300

    att

---
+++ Estudos/sessao026VueJS/montyhall/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <h1>Problema de Monty Hall</h1>
+    <GiftChosen />
+  </div>
+</template>
+
+<script>
+import GiftChosen from './components/GiftChosen'
+
+export default {
+    name: 'App',
+    components: { GiftChosen }
+
+}
+</script>
+
+<style>
+*{
+   box-sizing: border-box;
+   font-family: 'Montserrat', sans-serif; 
+}
+
+body{
+    color: #FFF;
+    background: linear-gradient(to right, rgb(21, 153, 87), rgb(21, 87, 153));
+}
+
+#app{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+#app h1 {
+    border: 1px solid #000;
+    background-color: #0004;
+    padding: 20px;
+    margin-bottom: 60px;
+}
+
+</style>
---
+++ Estudos/sessao026VueJS/montyhall/src/main.js
@@ -0,0 +1,11 @@
+import Vue, { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+new Vue({
+    render: h => h(App)
+    // render(createElement){
+    //     return createElement(App)
+    // }
+}).$mount("#app")
