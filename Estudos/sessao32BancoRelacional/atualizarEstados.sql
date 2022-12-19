495176d561545ca10415122c1e7393ecd3fd570c
Israel Ferreira <israel.fnds@hotmail.com>
2022-12-19 11:46:44 -0300

    att

--- Estudos/sessao32BancoRelacional/atualizarEstados.sql
+++ Estudos/sessao32BancoRelacional/atualizarEstados.sql
@@ -2,7 +2,7 @@ update estados set nome = 'Maranhão' where sigla = 'MA';
 
 select est.nome from estados est where sigla = 'MA';
 
-update estados set nome = 'Paraná', populacao = 11.32 where sigla = 'PR'
+update estados set sigla = 'SP' where nome = 'Campinas'
 
 select * from estados where sigla = 'PR'
 
