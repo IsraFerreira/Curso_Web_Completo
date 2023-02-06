3053a26b350b6f8a0c61df610ed64266e31ab768
Israel Ferreira <israel.fnds@hotmail.com>
2023-02-06 16:21:22 -0300

    att

--- Estudos/sessao35Knowledge/versao-inicial/backend/migrations/20230206102221_add_deleted_at_table_users.js
+++ Estudos/sessao35Knowledge/versao-inicial/backend/migrations/20230206102221_add_deleted_at_table_users.js
@@ -1,11 +1,11 @@
 
-exports.up = function(knex, Promise) {
+exports.up = function (knex, Promise) {
     return knex.schema.alterTable('users', table => {
         table.timestamp('deleteAt')
     })
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function (knex, Promise) {
     return knex.schema.alterTable('users', table => {
         table.dropColumn('deleteAt')
     })
