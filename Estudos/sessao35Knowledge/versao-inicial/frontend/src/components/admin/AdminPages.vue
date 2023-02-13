b9495fbd8ca569aa1e6a2626837d4125fbee3940
Israel Ferreira <israel.fnds@hotmail.com>
2023-02-13 16:38:31 -0300

    att

--- Estudos/sessao35Knowledge/versao-inicial/frontend/src/components/admin/AdminPages.vue
+++ Estudos/sessao35Knowledge/versao-inicial/frontend/src/components/admin/AdminPages.vue
@@ -6,13 +6,13 @@
             <b-card no-body>
                 <b-tabs card>
                     <b-tab title="Artigos" active>
-                        <ArticleAdmin />
+                        <!-- <ArticleAdmin /> -->
                     </b-tab>
                     <b-tab title="Categorias">
-                        <CategoryAdmin />
+                        <!-- <CategoryAdmin /> -->
                     </b-tab>
                     <b-tab title="Usuários">
-                        <UserAdmin />
+                        <!-- <UserAdmin /> -->
                     </b-tab>
                 </b-tabs>
             </b-card>
@@ -22,13 +22,10 @@
 
 <script>
 import PageTitle from '../template/PageTitle'
-import ArticleAdmin from './ArticleAdmin'
-import CategoryAdmin from './CategoryAdmin'
-import UserAdmin from './UserAdmin'
 
 export default {
     name: 'AdminPages',
-    components: { PageTitle, ArticleAdmin, CategoryAdmin, UserAdmin }
+    components: { PageTitle }
 }
 </script>
 
--- Estudos/sessao35Knowledge/versao-inicial/frontend/src/main.js
+++ Estudos/sessao35Knowledge/versao-inicial/frontend/src/main.js
@@ -6,10 +6,16 @@ import App from './App'
 
 import './config/bootstrap'
 import store from './config/store'
+import router from './config/router'
 
 Vue.config.productionTip = false
 
+
+require('axios').defaults.headers.common['Authorization'] = 
+'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IklzcmFlbCBGZXJyZWlyYSIsImVtYWlsIjoiaW5vbmF0b0BpZGVhcy5tZWQuYnIiLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNjc2MzE2Njg0LCJleHAiOjE2NzY1NzU4ODR9.iuO0dy56I9wZeCpLYbvOZUm7hbCSXfzqWiqIoRnBHnM'
+
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
