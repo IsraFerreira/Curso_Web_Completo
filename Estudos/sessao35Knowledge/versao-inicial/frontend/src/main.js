f3401cd3f2c25b291520342bbb418ef8bcc1c5cb
Israel Ferreira <israel.fnds@hotmail.com>
2023-02-24 11:48:49 -0300

    Update main.js

--- Estudos/sessao35Knowledge/versao-inicial/frontend/src/main.js
+++ Estudos/sessao35Knowledge/versao-inicial/frontend/src/main.js
@@ -13,7 +13,7 @@ Vue.config.productionTip = false
 
 
 require('axios').defaults.headers.common['Authorization'] = 
-'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IklzcmFlbCBGZXJyZWlyYSIsImVtYWlsIjoiaW5vbmF0b0BpZGVhcy5tZWQuYnIiLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNjc2MzE2Njg0LCJleHAiOjE2NzY1NzU4ODR9.iuO0dy56I9wZeCpLYbvOZUm7hbCSXfzqWiqIoRnBHnM'
+'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IklzcmFlbCBGZXJyZWlyYSIsImVtYWlsIjoiaW5vbmF0b0BpZGVhcy5tZWQuYnIiLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNjc3MjQ0NTc2LCJleHAiOjE2Nzc1MDM3NzZ9.AJMPIny-RD8IBjqJrw-7gj-lMV3K3-8wLjZ5-v1nRjo'
 
 new Vue({
   store,
