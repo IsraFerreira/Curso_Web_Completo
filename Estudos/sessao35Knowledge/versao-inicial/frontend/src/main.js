a0f28024695f4f01a6b11759a48b35a1eb82c438
Israel Ferreira <israel.fnds@hotmail.com>
2023-03-07 11:49:34 -0300

    att

--- Estudos/sessao35Knowledge/versao-inicial/frontend/src/main.js
+++ Estudos/sessao35Knowledge/versao-inicial/frontend/src/main.js
@@ -6,14 +6,14 @@ import App from './App'
 
 import './config/bootstrap'
 import './config/msgs'
+import './config/axios'
 import store from './config/store'
 import router from './config/router'
 
 Vue.config.productionTip = false
 
-
-require('axios').defaults.headers.common['Authorization'] = 
-'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IklzcmFlbCBGZXJyZWlyYSIsImVtYWlsIjoiaW5vbmF0b0BpZGVhcy5tZWQuYnIiLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNjc4MTAyMDYzLCJleHAiOjE2NzgzNjEyNjN9.1G6yqmnZ4ZnrkaQZ_UBTp1ZMlVtCwfagewQovh2jL7M'
+// require('axios').defaults.headers.common['Authorization'] = 
+// 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IklzcmFlbCBGZXJyZWlyYSIsImVtYWlsIjoiaW5vbmF0b0BpZGVhcy5tZWQuYnIiLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNjc4MTAyMDYzLCJleHAiOjE2NzgzNjEyNjN9.1G6yqmnZ4ZnrkaQZ_UBTp1ZMlVtCwfagewQovh2jL7M'
 
 new Vue({
   store,
